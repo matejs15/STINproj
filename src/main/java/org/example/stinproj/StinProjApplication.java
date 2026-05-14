48a01b5e2e1fb009c2e3d250018003d86cc592bb
Matěj Svoboda <matej.svoboda2@tul.cz>
2026-05-14 09:27:14 +0200

    Initial commit

---
+++ src/main/java/org/example/stinproj/StinProjApplication.java
@@ -0,0 +1,13 @@
+package org.example.stinproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StinProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StinProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/stinproj/StinProjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stinproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StinProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
